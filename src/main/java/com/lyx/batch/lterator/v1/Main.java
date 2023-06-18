bcba7316feaf228926992e176ac66464ebccda08
1297933624@qq.com <lilei@tvdcy.net>
2023-06-18 22:17:12 +0800

    迭代器模式

--- src/main/java/com/lyx/batch/lterator/v1/Main.java
+++ src/main/java/com/lyx/batch/lterator/v1/Main.java
@@ -1,49 +1,75 @@
 package com.lyx.batch.lterator.v1;
 
-import jdk.nashorn.internal.ir.CallNode;
-
 public class Main {
     public static void main(String[] args) {
-        Collection_ arrayList_ = new ArrayList_();
-        for(int i = 0 ; i < 15 ; i++){
-            String s = new String("a" + i);
+        Collection_<String> arrayList_ = new ArrayList_<String>();
+        for (int i = 0; i < 15; i++) {
+            String s = "a"+i;
             arrayList_.add(s);
+        }
+        Iterator_<String> iterator = arrayList_.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
         }
         System.out.println(arrayList_.size());
     }
 }
 
-interface Collection_ {
-    void add(Object o);
+interface Collection_<E> {
+    void add(E o);
+
     int size();
+
+    Iterator_ iterator();
 }
 
-class ArrayList_ implements Collection_{
-    Object[] objects = new Object[10];
+class ArrayList_<E> implements Collection_<E> {
+    E[] objects = (E[]) new Object[10];
     private int index = 0;
-    public void add(Object o){
-        if(index == objects.length){
-            Object[] newObjects = new Object[this.objects.length * 2];
-            System.arraycopy(objects,0,newObjects,0,objects.length);
+
+    public void add(E o) {
+        if (index == objects.length) {
+            E[] newObjects = (E[]) new Object[this.objects.length * 2];
+            System.arraycopy(objects, 0, newObjects, 0, objects.length);
             objects = newObjects;
         }
-        objects[index++] = o;
+        objects[index] = o;
+        index++;
     }
-    public int size(){
+
+    public int size() {
         return objects.length;
+    }
+
+    @Override
+    public Iterator_<E> iterator() {
+        return new ArrayListIterator();
+    }
+
+    private class ArrayListIterator<E> implements Iterator_<E> {
+        private int currentIndex = 0;
+        @Override
+        public boolean hasNext() {
+            return !(currentIndex >= index);
+        }
+
+        @Override
+        public E next() {
+            return (E) objects[currentIndex++];
+        }
     }
 }
 
-class LinkedList_ implements Collection_{
+class LinkedList_<E> implements Collection_<E> {
     Node head = null;
     Node tail = null;
     private int size = 0;
 
-    public void add(Object o){
+    public void add(E o) {
         Node n = new Node(o);
         n.next = null;
 
-        if(head == null){
+        if (head == null) {
             head = n;
             tail = n;
         }
@@ -52,25 +78,42 @@ class LinkedList_ implements Collection_{
         size++;
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
 
+    @Override
+    public Iterator_ iterator() {
+        return null;
+    }
 
+    private class LinkedListIter<E> implements Iterator_<E>{
+        Node curr = head;
+        @Override
+        public boolean hasNext() {
+            return curr != null;
+        }
 
+        @Override
+        public E next() {
+            return (E) (curr = curr.next);
+        }
+    }
 
-
-    private class Node {
-        private Object o;
+    private class Node<E> {
+        private E o;
         Node next;
 
-        public Node(Object o) {
+        public Node(E o) {
             this.o = o;
         }
     }
 
+}
+interface Iterator_<E> {
+    boolean hasNext();
 
-
+    E next();
 }
 
 
